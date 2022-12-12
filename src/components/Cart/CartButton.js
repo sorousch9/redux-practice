545d11869a5bac2f4d4761cf2636ce046ac6cffa
Soroush Safarkhah <78909837+sorousch9@users.noreply.github.com>
2022-12-12 10:49:08 +0100

    cart reducer created

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,17 +1,17 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { uiActions } from "../../store/ui-slice";
 import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
   const toggleHandler = () => {
-    dispatch(uiActions.toggle())
+    dispatch(uiActions.toggle());
   };
-
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
   return (
     <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,45 @@
 import ProductItem from "./ProductItem";
 import classes from "./Products.module.css";
-
+const DummyProduct = [
+  {
+    id: "p1",
+    price: 59,
+    title: "Johnny Urban Rucksack",
+    description: "Volume: 12.5 l (Size One Size)",
+  },
+  {
+    id: "p2",
+    price: 33,
+    title: "Carhartt WIP Rucksack",
+    description: "Height: 22 cm (Size One Size),Length: 16 cm (Size One Size)",
+  },
+  {
+    id: "p3",
+    price: 18,
+    title: "Nike Sportswear Across body bag",
+    description: "Height: 24 cm (Size One Size),Length: 19 cm (Size One Size)",
+  },
+  {
+    id: "p4",
+    price: 26,
+    title: "The North Face JESTER ",
+    description: "Height: 20 cm (Size One Size),Length: 15 cm (Size One Size)",
+  },
+];
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title="Test"
-          price={6}
-          description="This is a first product - amazing!"
-        />
+        {DummyProduct.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
